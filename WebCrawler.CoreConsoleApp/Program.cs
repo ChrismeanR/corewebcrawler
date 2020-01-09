b59b9f0a486951978069a07b569f6477ec908718
Christine Romano <chrismean@gmail.com>
2020-01-09 14:44:45 -0700

    Changing things, updating data returns, tweaking context a tad

--- WebCrawler.CoreConsoleApp/Program.cs
+++ WebCrawler.CoreConsoleApp/Program.cs
@@ -10,7 +10,8 @@ namespace WebCrawler.CoreConsoleApp
     {
         public static string gobjDomain = "connectforhealthco.com";
         public static List<List<DisplayModel>> gobjPageOutput = new List<List<DisplayModel>>();
-        //protected static ListOfDisplay<DisplayModel> gobjPageOutput = new ListOfDisplay<DisplayModel>();
+        public static Dictionary<string, List<Uri>> gobjLinks = new Dictionary<string, List<Uri>>();
+
         public static void Main(string[] args)
         {
             Console.WriteLine("Start crawling");
@@ -37,6 +38,8 @@ namespace WebCrawler.CoreConsoleApp
             var cssLinks = doc.DocumentNode.SelectNodes("//link[@rel]");
             var atagLinks = doc.DocumentNode.SelectNodes("//a[@href]");
 
+            var objLinks = GetNodeAttributesByTag(doc, atagLinks);
+
             // run these through the function and add them to the global list
             var pageColReturn = GetNodeAttributesByTag(atagLinks, "href", "a");
             var contentColReturn = GetNodeAttributesByTag(images, "src", "img");
@@ -60,22 +63,81 @@ namespace WebCrawler.CoreConsoleApp
 
             // format this as a return by combining the above
 
-            foreach (var item in itemsList)
+            //foreach (var item in itemsList)
+            //{
+            //    foreach (var detail in item)
+            //    {
+            //        // TODO: if more time, get this hierarchy correct with relative urls & page content. It appends to the end currently
+
+            //        //if (detail.PageUri.Contains(gobjDomain))
+            //        //{
+            //        //    Console.WriteLine(detail.PageUri);
+            //        //}
+            //        //else
+            //        //{
+            //        Console.WriteLine(detail.PageUri);
+            //        //}
+            //    }
+            //}
+            foreach (var item in objLinks)
             {
+                Console.WriteLine(item);
                 foreach (var detail in item)
                 {
-                    // TODO: if more time, get this hierarchy correct with relative urls & page content. It appends to the end currently
-
-                    //if (detail.PageUri.Contains(gobjDomain))
-                    //{
-                    //    Console.WriteLine(detail.PageUri);
-                    //}
-                    //else
-                    //{
-                    Console.WriteLine(detail.PageUri);
-                    //}
+                    Console.WriteLine(detail);
+                    //Console.WriteLine($"Page: {detail.Key}", $"Url: {detail.Value}");
                 }
             }
+        }
+
+        public static List<Dictionary<string, Uri>> GetNodeAttributesByTag(HtmlDocument doc, HtmlNodeCollection nodes)
+        {
+            List<Uri> linkList = new List<Uri>();
+            var htmlAttributes = nodes.Select(x => x.Attributes);
+            Uri validUri;
+
+            Dictionary<string, Uri> outputList = new Dictionary<string, Uri>();
+
+            var colReturn = new List<Dictionary<string, Uri>>();
+
+            foreach (HtmlNode node in nodes)
+            {
+                var attribute = node.Attributes["href"];
+                if(attribute != null && attribute.Value.Contains("a"))
+                {
+
+                }
+            }
+            for (int i = 0; i < nodes.Count; i++)
+            {
+                Console.WriteLine();
+
+                #region MyRegion
+                //foreach (var item in nodes)
+                //{
+                //    if (item.OriginalName.StartsWith('a') != null)
+                //    {
+                //        // pull the URL, output all children
+                //        if (item.GetAttributeValue("href", "null") != null)
+                //        {
+                //            if (Uri.IsWellFormedUriString(item.Attributes["href"].Value, UriKind.Absolute))
+                //            {
+                //                validUri = new Uri(item.Attributes["href"].Value, UriKind.RelativeOrAbsolute);
+                //                //linkList.Add(validUri);
+                //                if (!outputList.ContainsKey(item.GetAttributeValue("title", def: "title " + i) ))
+                //                {
+                //                    outputList.TryAdd(item.GetAttributeValue("title", def: "title " + i), validUri);
+                //                }
+                //            }
+                //        }
+                //    }
+
+                //} 
+                #endregion
+            }
+            colReturn.Add(outputList);
+
+            return colReturn;
         }
 
         public static List<List<DisplayModel>> GetNodeAttributesByTag(HtmlNodeCollection colHtmlNode, string strAttribute, string tagType)
